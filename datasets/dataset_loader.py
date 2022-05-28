1219b5e15efe5e83fcfb10b8e2bb114ad722dd89
johnMinelli <giovanni.minelli3@gmail.com>
2022-05-28 10:19:33 +0200

    test nvs dataloader

--- datasets/dataset_loader.py
+++ datasets/dataset_loader.py
@@ -1,7 +1,7 @@
 ﻿def CreateDataset(opt, train=False, valid=False, eval=False):
     if opt.dataset in ['shapenet']:
         from datasets.shapenet_data_loader import ShapeNetDataset
-        dataset = ShapeNetDataset(opt, train, valid, eval)
+        dataset = ShapeNetDataset(opt)
     elif opt.dataset in ['kitti']:
         from datasets.kitti_data_loader import KITTIDataset
         dataset = KITTIDataset(opt, train, valid, eval)
